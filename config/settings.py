c88182b9b9b53d333752d68cbe27f1a4597c1b6b
gcsamuel <gcsamuelsena@gmail.com>
2026-05-19 17:51:04 -0300

    settings atualizado

--- config/settings.py
+++ config/settings.py
@@ -29,13 +29,17 @@ SECRET_KEY = config(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+#DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config(
-    'ALLOWED_HOSTS',
-    default='127.0.0.1,localhost',
-    cast=Csv()
-)
+#ALLOWED_HOSTS = config(
+ #   'ALLOWED_HOSTS',
+  #  default='127.0.0.1,localhost', ''
+   # cast=Csv()
+#)
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "192.168.0.118", 'Samuca.pythonanywhere.com']
 
 
 # Application definition
